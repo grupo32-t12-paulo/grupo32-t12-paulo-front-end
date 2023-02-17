7b1607cad0f6725d9193cc1781a7dfb2ec45ec89
wendelbueres <wendelbueres@gmail.com>
2023-02-16 21:42:58 -0300

    refactor: :hammer: GalleryGrid

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,72 +1,93 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Photo from "../../assets/photo_example.png";
-import { Card, CardMedia } from "@mui/material";
-import { Button, Typography } from "@material-ui/core";
-import { useNavigate } from "react-router";
-import { useState } from "react";
+import Photo2 from "../../assets/photo_example2.png";
+import Photo3 from "../../assets/photo_example3.png";
+import { Card, CardActionArea, CardMedia } from "@mui/material";
+import { Typography } from "@material-ui/core";
+import { useContext, useEffect } from "react";
+import { CurrentImageGalleryContext } from "../../contexts/gallery.context";
+import { Container } from "@mui/system";
 
 export default function GalleryGrid() {
-  const photos = [Photo, Photo, Photo, Photo, Photo, Photo];
+  const photos = [Photo, Photo2, Photo3, Photo, Photo2, Photo3];
+  const { setCurrentImage, setOpen } = useContext(CurrentImageGalleryContext);
 
-  const [currentImage, setCurrentImage] = useState(photos[0]);
+  useEffect(() => {
+    setOpen(false);
+    setCurrentImage(photos[0]);
+  }, []);
+
   return (
-    <Box
-      sx={{
-        width: "440px",
-        height: "377px",
-        padding: "36px 44px",
-        borderRadius: "4px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-      }}
-    >
-      <Typography
-        variant="h5"
-        style={{
-          color: "#212529",
-          fontWeight: 600,
-          fontSize: "20px",
-          fontFamily: "Lexend, sans-serif",
+    <Container sx={{ mt: 5 }}>
+      <Box
+        sx={{
+          width: { xs: "100%", md: "440px" },
+          height: { md: "377px" },
+          borderRadius: "4px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
         }}
       >
-        Fotos
-      </Typography>
-      <Grid container spacing={2}>
-        {photos.map((photo) => (
-          <Grid item xs={4}>
-            <Button onClick={() => setCurrentImage(photo)}>
-              <Card
+        <Typography
+          variant="h5"
+          style={{
+            color: "#212529",
+            fontWeight: 600,
+            fontSize: "20px",
+            fontFamily: "Lexend, sans-serif",
+          }}
+        >
+          Fotos
+        </Typography>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            p: { xs: "18px 26px", md: "36px 44px" },
+          }}
+        >
+          {photos.map((photo, index) => (
+            <Grid key={`photo_${index}`} item xs={4}>
+              <CardActionArea
+                onClick={() => setCurrentImage(photo)}
                 sx={{
-                  width: 108,
-                  maxWidth: 108,
-                  height: 108,
-                  maxHeight: 108,
-                  backgroundColor: "#E9ECEF",
-                  padding: 1,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  userSelect: "none",
+                  width: { xs: "100", md: 108 },
+                  height: { xs: "100", md: 108 },
                 }}
               >
-                <CardMedia
+                <Card
                   sx={{
-                    overflow: "visible",
-                    objectFit: "contain",
-                    minWidth: "100",
-                    pointerEvents: "none",
+                    width: { xs: "100", md: 108 },
+                    height: { xs: "100", md: 108 },
+                    backgroundColor: "#E9ECEF",
+                    padding: 0,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
                     userSelect: "none",
                   }}
-                  component="img"
-                  image={photo}
-                />
-              </Card>
-            </Button>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+                >
+                  <CardMedia
+                    sx={{
+                      overflow: "visible",
+                      objectFit: "contain",
+                      pointerEvents: "none",
+                      userSelect: "none",
+                    }}
+                    component="img"
+                    image={photo}
+                  />
+                </Card>
+              </CardActionArea>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
   );
 }
