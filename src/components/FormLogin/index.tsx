b1961080e58d952b622e018eb28ab560e10a734a
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-24 14:49:16 -0300

    Feat: Added yup and use context

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,18 +1,46 @@
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { Box } from "@mui/material";
+import { useContext } from "react";
+import { LoginContext } from "../../contexts/login.context";
 import { ContainerRoot, Div, Form } from "./style";
 
 const Login = () => {
+  const { signIn } = useContext(LoginContext)
+
+  const schema = yup.object({
+    email: yup.string().required("O email é obrigatório").email("Email incorreto"),
+    password: yup.string().required("Senha é obrigatório")
+  });
+
+  interface IForm {
+    email: string;
+    password: string;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    resolver: yupResolver(schema),
+  });
+
   return (
     <Box sx={{mt: {md : 10, xs : 18}, mb: {md: 0, xs: 10}}}>
     <ContainerRoot>
       <Div>
-        <Form>
+        <Form onSubmit={handleSubmit(signIn)}>
           <h2>Login</h2>
           <label htmlFor="user">Usuário</label>
-          <input type="text" id="user" placeholder="Digitar usuário" />
+          <input {...register("email")} type="text" id="user" placeholder="Digitar usuário" />
+          {errors.email && <p>{errors.email.message}</p>}
 
           <label htmlFor="password">Senha</label>
-          <input type="password" id="password" placeholder="Digitar senha" />
+          <input {...register("password")} type="password" id="password" placeholder="Digitar senha" />
+          {errors.password && <p>{errors.password.message}</p>}
 
           <span>Esqueci minha senha</span>
 
