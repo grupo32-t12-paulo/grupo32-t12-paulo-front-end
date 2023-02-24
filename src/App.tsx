b2f020578f1fb202010c26188308c430dc6486bf
wendelbueres <wendelbueres@gmail.com>
2023-02-24 05:11:03 -0300

    feat: :sparkles: importados componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Global from "./style/Global";
 import RouteApllication from "./routes";
+import ProductPage from "./pages/product";
+import ModalExpandImage from "./components/ModalExpandImage";
 
 function App() {
   return (
     <>
       <Global />
+      <ModalExpandImage />
       <RouteApllication />
+      <ProductPage />
     </>
   );
 }
