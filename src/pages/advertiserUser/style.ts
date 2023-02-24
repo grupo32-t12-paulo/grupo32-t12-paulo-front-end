729af7cac49eb1ea5b093e5a951157ee8f3fb1e0
wendelbueres <wendelbueres@gmail.com>
2023-02-24 02:52:05 -0300

    style: :umbrella: removidos alguns items

--- src/pages/advertiserUser/style.ts
+++ src/pages/advertiserUser/style.ts
@@ -1,34 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const ContainerRoot = styled.div`
   background: linear-gradient(
     180deg,
     rgba(69, 41, 230, 1) 15%,
     rgba(248, 249, 250, 1) 15%
   );
-
-  .action {
-    display: none;
-  }
-
-  h2 {
-    font-size: 1.2rem;
-    font-weight: var(--Heading-2-600);
-    margin: 20px;
-  }
-
-  ul {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    overflow-x: scroll;
-    gap: 30px;
-    margin: 0px 20px;
-  }
-
-  li {
-    list-style: none;
-  }
 
   @media (min-width: 769px) {
     background: linear-gradient(
@@ -36,17 +13,6 @@ export const Container = styled.div`
       rgba(69, 41, 230, 1) 15%,
       rgba(248, 249, 250, 1) 15%
     );
-  }
-
-  .action,
-  h2 {
-    display: flex;
-    margin: 6vw;
-  }
-
-  ul {
-    gap: 20px;
-    margin: 0px 50px;
   }
 `;
 
