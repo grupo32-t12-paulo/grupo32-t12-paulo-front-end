5233ff4dc990e451afcb07928606d03bf6f4b298
wendelbueres <wendelbueres@gmail.com>
2023-02-28 18:37:53 -0300

    feat: :sparkles: adicionada rota announcement

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import AdvertiserUser from "../pages/advertiserUser";
+import ProductPage from "../pages/product";
 
 const RouteApllication = () => {
   return (
     <Routes>
       <Route path="/users" />
       <Route path="advertiser-profile" element={<AdvertiserUser />} />
+      <Route path="/announcement/:id" element={<ProductPage />} />
     </Routes>
   );
 };
