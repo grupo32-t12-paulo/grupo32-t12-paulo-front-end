a4fa917300d300bfe54e9977351b842c59f945e7
wendelbueres <wendelbueres@gmail.com>
2023-02-28 18:49:35 -0300

    fix: :bug: erro no merger

--- src/components/CardAuction/index.tsx
+++ src/components/CardAuction/index.tsx
@@ -9,107 +9,82 @@ import VehicleData from "../VehicleData";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import { AvatarComponent } from "../Avatar";
 
-
 export default function ActionAreaCard() {
-    return (
-        <Card sx={{ maxWidth: 735 }}
-        >
-            <CardActionArea
-                style={
-                    {
-                        display: "flex",
-                        minHeight: "326px",
-                        alignItems: "center",
-                        justifyContent: "center",
-                    }}
-            >
-                <CardContent
-                    style={
-                        {
-                            backgroundImage: `linear-gradient(180deg,rgba(0,0,0,0.29) 0%, #000000 100%),
+  return (
+    <Card sx={{ maxWidth: 735 }}>
+      <CardActionArea
+        style={{
+          display: "flex",
+          minHeight: "326px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CardContent
+          style={{
+            backgroundImage: `linear-gradient(180deg,rgba(0,0,0,0.29) 0%, #000000 100%),
                             url("https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/02-images/novo-cruze-nb.jpg?imwidth=960"
                             `,
-                            backgroundSize: "contain",
-                            backgroundRepeat: "no-repeat",
-                            display: 'flex',
-                            flexDirection: "column",
-                            width: "100%",
-                            minHeight: "326px",
-                            justifyContent: "space-around",
-                            boxSizing: "border-box",
-                            padding: "10 20px",
-                        }}
-                >
-                    <div
-                        style={
-                            {
-                                width: "123px",
-                                height: "36px",
-                                display: "flex",
-                                alignItems: "center",
-                                padding: "0 2px",
-                                boxSizing: "border-box",
-                                justifyContent: "space-around",
-                                borderRadius: "32px",
-                                background: "#FFFFFF"
-                            }}
-                    >
-                        <AccessTimeIcon style={{ color: "#4529E6" }} />
-                        <span style={{ fontSize: "16px" }}>01:58:00</span>
-                    </div>
-                    <div>
-
-                        <Typography>
-                            <h3
-                                style={
-                                    {
-                                        color: "#FDFDFD",
-                                        fontSize: "20px",
-                                    }}
-                            >
-                                Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200</h3>
-                            <p style={{ color: "#CED4DA", fontSize: "16px" }}>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...
-                            </p>
-                        </Typography>
-
-                        <Typography
-                            style={
-                                {
-                                    width: "100%",
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    justifyContent: "space-between",
-                                    gap: "20px"
-                                }}
-                        >
-                            <AvatarComponent backgroundA="#4529E6" colorP="#FFFFFF" />
-                            <VehicleData colorP="#FFFFFF" colorSpanYQ="#4529E6" backgroundYQ="#EDEAFD" />
-                        </Typography>
-                    </div>
-                </CardContent>
-            </CardActionArea>
-            <Button variant="contained" style={
-                {
-                    display: "flex",
-                    justifyContent: "space-between",
-                    width: "100%",
-                    height: "62px",
-                    background: "#4529E6",
-                    fontSize: "16px",
-                    padding: "0 20px",
-                    borderTopLeftRadius: "0",
-                    WebkitBorderTopRightRadius: "0",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            minHeight: "326px",
+            justifyContent: "space-around",
+            boxSizing: "border-box",
+            padding: "10 20px",
+          }}
+        >
+          <div
+            style={{
+              width: "123px",
+              height: "36px",
+              display: "flex",
+              alignItems: "center",
+              padding: "0 2px",
+              boxSizing: "border-box",
+              justifyContent: "space-around",
+              borderRadius: "32px",
+              background: "#FFFFFF",
+            }}
+          >
+            <AccessTimeIcon style={{ color: "#4529E6" }} />
+            <span style={{ fontSize: "16px" }}>01:58:00</span>
+          </div>
+          <div>
+            <Typography>
+              <h3
+                style={{
+                  color: "#FDFDFD",
+                  fontSize: "20px",
                 }}
+              >
+                Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200
+              </h3>
+              <p style={{ color: "#CED4DA", fontSize: "16px" }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem...
+              </p>
+            </Typography>
+
+            <Typography
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                gap: "20px",
+              }}
             >
               <AvatarComponent backgroundA="#4529E6" colorP="#FFFFFF" />
               <VehicleData
                 colorP="#FFFFFF"
                 colorSpanYQ="#4529E6"
                 backgroundYQ="#EDEAFD"
-                mileage={0}
-                price={"150.000"}
-                year={2020}
+                mileage={4000}
+                price={"8000"}
+                year={1999}
               />
             </Typography>
           </div>
@@ -135,7 +110,3 @@ export default function ActionAreaCard() {
     </Card>
   );
 }
-
-
-
-
