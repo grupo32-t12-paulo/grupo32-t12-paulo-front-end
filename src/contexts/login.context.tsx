2e0516164bbc55cb4d74407f7198debac2d8cd38
wendelbueres <wendelbueres@gmail.com>
2023-03-01 17:12:27 -0300

    feat: :sparkles: token decodificado

--- src/contexts/login.context.tsx
+++ src/contexts/login.context.tsx
@@ -1,4 +1,5 @@
 import api from "../services/api";
+import decode from "jwt-decode";
 import { toast } from "react-toastify";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
@@ -11,15 +12,41 @@ interface ILoginContext {
   signIn: SubmitHandler<FieldValues>;
 }
 
+interface IAddress {
+  cep: string;
+  state: string;
+  city: string;
+  street: string;
+  number: number;
+  complement?: string;
+}
+
 export interface IUser {
-  id: number;
+  name: string;
   email: string;
-  password: string;
+  password?: string;
+  cpf: string;
+  cellPhone: string;
+  dateBirth: Date | string | number;
+  description: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  idAdvertiser: boolean;
+  address: IAddress;
+  id: number;
+  addressId: number;
 }
 
 export interface IResponseLogin {
   user: IUser;
   token: string;
+}
+
+interface IToken {
+  id: string;
+  isActive: boolean;
+  isAdvertiser: boolean;
 }
 
 export const LoginContext = createContext<ILoginContext>({} as ILoginContext);
@@ -33,13 +60,16 @@ export function LoginProvider({ children }: { children: React.ReactNode }) {
       const response = await api.post<IResponseLogin>("/login", data);
 
       if (response.status === 200) {
-        const { user, token } = response.data;
+        const { token } = response.data;
+        const resJWT: IToken = decode(token);
+
         localStorage.setItem("@motorshop:token", token);
 
         api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        const { data } = await api.get(`/users/${resJWT.id}`);
 
-        setUser(user);
-        navigate("/announcement/10");
+        setUser(data);
+        navigate("/announcement/1");
       }
     } catch (err) {
       toast.error("Usuário ou senha incorreto.");
