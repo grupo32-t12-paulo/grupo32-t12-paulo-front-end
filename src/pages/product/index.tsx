546f5c90d1765c4a04405525a23e9fa4433f2efd
wendelbueres <wendelbueres@gmail.com>
2023-03-01 02:37:00 -0300

    feat: :sparkles: renderizações condicionais

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,6 +1,6 @@
 import { Box, CircularProgress, Grid } from "@material-ui/core";
 import { Container } from "@mui/joy";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AppBarComponent from "../../components/AppBar";
 import CardDescriptionProduct from "../../components/CardDescriptionProduct";
@@ -15,82 +15,101 @@ import { AnnouncementDetailsContext } from "../../contexts/announcementDetails.c
 import { ContainerRoot } from "./style";
 
 const ProductPage = () => {
+  const [conditional, setConditional] = useState<true | false>(false);
   const { setId, announcement, comments } = useContext(
     AnnouncementDetailsContext
   );
   const { id } = useParams();
   setId(id);
 
-  if (announcement && comments) {
-    return (
-      <>
-        <AppBarComponent />
-        <ContainerRoot>
-          <Container sx={{ display: { md: "flex", xs: "none" } }}>
-            <Box>
-              <Grid container spacing={2}>
-                <Grid item xs={8}>
-                  <CurrentImageGallery />
-                  <CardTitleProduct
-                    title={announcement.title}
-                    year={announcement.year}
-                    mileage={announcement.mileage}
-                    price={announcement.price}
-                  />
-                  <CardDescriptionProduct
-                    description={announcement.description}
-                  />
-                  <ListComments comments={comments} />
-                  <InputComment />
-                </Grid>
-                <Grid item xs={4}>
-                  <GalleryGrid />
+  useEffect(() => {
+    if (announcement && comments) {
+      setConditional(true);
+    } else {
+      setConditional(false);
+    }
+  }, [announcement, comments]);
+
+  return conditional ? (
+    <>
+      <AppBarComponent />
+      <ContainerRoot>
+        <Container sx={{ display: { md: "flex", xs: "none" } }}>
+          <Box>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+                <CurrentImageGallery />
+                {announcement && (
+                  <>
+                    <CardTitleProduct
+                      title={announcement.title}
+                      year={announcement.year}
+                      mileage={announcement.mileage}
+                      price={announcement.price}
+                    />
+                    <CardDescriptionProduct
+                      description={announcement.description}
+                    />
+                  </>
+                )}
+                {comments && <ListComments comments={comments} />}
+                <InputComment />
+              </Grid>
+              <Grid item xs={4}>
+                <GalleryGrid />
+                {announcement && (
                   <CardSellerInfo
                     name={announcement.user.name}
                     description={announcement.user.description}
                   />
-                </Grid>
+                )}
               </Grid>
-            </Box>
-          </Container>
-          <Container sx={{ display: { md: "none", xs: "flex" } }}>
-            <Box>
-              <CurrentImageGallery />
-              <CardTitleProduct
-                title={announcement.title}
-                year={announcement.year}
-                mileage={announcement.mileage}
-                price={announcement.price}
-              />
-              <CardDescriptionProduct description={announcement.description} />
-              <GalleryGrid />
+            </Grid>
+          </Box>
+        </Container>
+        <Container sx={{ display: { md: "none", xs: "flex" } }}>
+          <Box>
+            <CurrentImageGallery />
+            {announcement && (
+              <>
+                <CardTitleProduct
+                  title={announcement.title}
+                  year={announcement.year}
+                  mileage={announcement.mileage}
+                  price={announcement.price}
+                />
+                <CardDescriptionProduct
+                  description={announcement.description}
+                />
+              </>
+            )}
+            <GalleryGrid />
+            {announcement && (
               <CardSellerInfo
                 name={announcement.user.name}
                 description={announcement.user.description}
               />
-              <ListComments comments={comments} />
-              <InputComment />
-            </Box>
-          </Container>
-          <BackToTop />
-        </ContainerRoot>
-      </>
-    );
-  } else {
-    return (
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          justifyItems: "center",
-          height: "100vh",
-        }}
-      >
-        <CircularProgress />
-      </Container>
-    );
-  }
+            )}
+            {comments && <ListComments comments={comments} />}
+            <InputComment />
+          </Box>
+        </Container>
+        <BackToTop />
+      </ContainerRoot>
+    </>
+  ) : (
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        justifyItems: "center",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+    </Container>
+  );
 };
 
 export default ProductPage;
