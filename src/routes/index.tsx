b46ed838dcc7ac88cd8aec74a4dc8af6f430594d
wendelbueres <wendelbueres@gmail.com>
2023-03-01 02:37:13 -0300

    feat: :sparkles: adicionada rota login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AdvertiserUser from "../pages/advertiserUser";
 import ProductPage from "../pages/product";
 import HomePage from "../pages/homePage";
+import LoginPage from "../pages/loginPage";
 
 const RouteApllication = () => {
   return (
@@ -9,6 +10,7 @@ const RouteApllication = () => {
       <Route path="/homepage" element={<HomePage />} />
       <Route path="advertiser-profile" element={<AdvertiserUser />} />
       <Route path="/announcement/:id" element={<ProductPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
