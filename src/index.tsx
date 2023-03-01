7705dda96861d792e30b777241baac00cf849daa
wendelbueres <wendelbueres@gmail.com>
2023-03-01 02:33:23 -0300

    feat: :sparkles: importado loginProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "react-toastify/dist/ReactToastify.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { CurrentImageGalleryProvider } from "./contexts/gallery.context";
 import UserProvider from "./contexts/user.context";
 import { AnnouncementDetailsProvider } from "./contexts/announcementDetails.context";
 import AnnouncementProvider from "./contexts/announcement.context";
+import { LoginProvider } from "./contexts/login.context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,25 +19,27 @@ root.render(
   <React.StrictMode>
     <Router>
       <CurrentImageGalleryProvider>
-        <UserProvider>
-          <AnnouncementDetailsProvider>
-            <AnnouncementProvider>
-              <ToastContainer
-                position="top-right"
-                autoClose={4000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-              />
-              <App />
-            </AnnouncementProvider>
-          </AnnouncementDetailsProvider>
-        </UserProvider>
+        <LoginProvider>
+          <UserProvider>
+            <AnnouncementDetailsProvider>
+              <AnnouncementProvider>
+                <ToastContainer
+                  position="top-right"
+                  autoClose={4000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
+                <App />
+              </AnnouncementProvider>
+            </AnnouncementDetailsProvider>
+          </UserProvider>
+        </LoginProvider>
       </CurrentImageGalleryProvider>
     </Router>
   </React.StrictMode>
