1f061fad9706fcd4fb1f191ee13a93b495e8f68d
Everton Vieira <vieiraeverton33@hotmail.com>
2023-03-08 09:33:56 -0300

    feat: adicionado navigate e rota default

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AdvertiserUser from "../pages/advertiserUser";
 import ProductPage from "../pages/product";
 import HomePage from "../pages/homePage";
@@ -22,6 +22,7 @@ const RouteApllication = () => {
       <Route path="/profileAdmin/:id" element={<ProfileViewAdmin />} />
       <Route path="/announcement/:id" element={<ProductPage />} />
       <Route path="/recover-password/:id/:token" element={<ResetPassword />} />
+      <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
 };
