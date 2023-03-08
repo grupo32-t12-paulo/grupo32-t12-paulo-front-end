7d4e55d9c7cab43a6b613ff7632da89486f17dba
wendelbueres <wendelbueres@gmail.com>
2023-03-08 13:42:54 -0300

    fix: :bug: removido conflito de importação

--- src/components/CreateAnnouncement/index.tsx
+++ src/components/CreateAnnouncement/index.tsx
@@ -11,130 +11,145 @@ interface IProps {
 }
 
 const CreateAnnouncement = ({ annoucementId }: IProps) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
   return (
     <>
-      {
-        open ?
-          <Container>
-            <Div>
-              <Modal>
-                <Header>
-                  <h2>Editar anúncio</h2>
-                  <button className="buttonExit"
-                    onClick={() => setOpen(false)}
-                  >X</button>
-                </Header>
+      {open ? (
+        <Container>
+          <Div>
+            <Modal>
+              <Header>
+                <h2>Editar anúncio</h2>
+                <button className="buttonExit" onClick={() => setOpen(false)}>
+                  X
+                </button>
+              </Header>
 
-                <Form>
-                  <h3>Tipo de anúncio</h3>
-                  <div className="sale-auction">
-                    <button className="buttonSale">Venda</button>
-                    <button className="buttonAuction">Leilão</button>
-                  </div>
+              <Form>
+                <h3>Tipo de anúncio</h3>
+                <div className="sale-auction">
+                  <button className="buttonSale">Venda</button>
+                  <button className="buttonAuction">Leilão</button>
+                </div>
 
-                  <h3>Informações do veículo</h3>
+                <h3>Informações do veículo</h3>
 
-                  <label htmlFor="title">Título</label>
-                  <input type="text" id="title" placeholder="Digitar título" />
+                <label htmlFor="title">Título</label>
+                <input type="text" id="title" placeholder="Digitar título" />
 
-                  <div className="year-mileage-price">
-                    <div className="div-year-mileage">
-                      <div className="divYear">
-                        <label htmlFor="year">Ano</label>
-                        <input
-                          className="year"
-                          type="number"
-                          id="year"
-                          placeholder="Digitar ano"
-                        />
-                      </div>
-
-                      <div className="divMileage">
-                        <label htmlFor="mileage">Quilometragem</label>
-                        <input
-                          className="mileage"
-                          type="number"
-                          id="mileage"
-                          placeholder="Digitar quilometragem"
-                        />
-                      </div>
+                <div className="year-mileage-price">
+                  <div className="div-year-mileage">
+                    <div className="divYear">
+                      <label htmlFor="year">Ano</label>
+                      <input
+                        className="year"
+                        type="number"
+                        id="year"
+                        placeholder="Digitar ano"
+                      />
                     </div>
 
-                    <div className="divPrice">
-                      <label htmlFor="price">Preço</label>
+                    <div className="divMileage">
+                      <label htmlFor="mileage">Quilometragem</label>
                       <input
-                        className="price"
+                        className="mileage"
                         type="number"
-                        id="price"
-                        placeholder="Digitar preço"
+                        id="mileage"
+                        placeholder="Digitar quilometragem"
                       />
                     </div>
                   </div>
 
-                  <label htmlFor="description">Descrição</label>
-                  <textarea id="description" placeholder="Digitar descrição" />
-
-                  <h3>Tipo de veículo</h3>
-                  <div className="car-motorcycle">
-                    <button className="buttonCar">Carro</button>
-                    <button className="buttonMotorcycle">Moto</button>
+                  <div className="divPrice">
+                    <label htmlFor="price">Preço</label>
+                    <input
+                      className="price"
+                      type="number"
+                      id="price"
+                      placeholder="Digitar preço"
+                    />
                   </div>
+                </div>
 
-                  <label htmlFor="coverImage">Imagem da capa</label>
-                  <input type="text" id="coverImage" placeholder="https://image.com" />
+                <label htmlFor="description">Descrição</label>
+                <textarea id="description" placeholder="Digitar descrição" />
 
-                  <label htmlFor="firstImage">1ª imagem da galeria</label>
-                  <input type="text" id="firstImage" placeholder="https://image.com" />
+                <h3>Tipo de veículo</h3>
+                <div className="car-motorcycle">
+                  <button className="buttonCar">Carro</button>
+                  <button className="buttonMotorcycle">Moto</button>
+                </div>
 
-                  <label htmlFor="secondImage">2ª imagem da galeria</label>
-                  <input type="text" id="secondImage" placeholder="https://image.com" />
+                <label htmlFor="coverImage">Imagem da capa</label>
+                <input
+                  type="text"
+                  id="coverImage"
+                  placeholder="https://image.com"
+                />
 
-                  <span>Adicionar campo para imagem da galeria</span>
+                <label htmlFor="firstImage">1ª imagem da galeria</label>
+                <input
+                  type="text"
+                  id="firstImage"
+                  placeholder="https://image.com"
+                />
 
-                  <div className="cancel-createContente">
-                    <ModalDelete annoucementId={annoucementId} />
-                    {/* <button className="buttonCancel" onClick={() => ""}>
+                <label htmlFor="secondImage">2ª imagem da galeria</label>
+                <input
+                  type="text"
+                  id="secondImage"
+                  placeholder="https://image.com"
+                />
+
+                <span>Adicionar campo para imagem da galeria</span>
+
+                <div className="cancel-createContente">
+                  <ModalDelete annoucementId={annoucementId} />
+                  {/* <button className="buttonCancel" onClick={() => ""}>
                       Excluir anúncios
                     </button> */}
-                    <button className="buttonCreateContente">Salvar alterações</button>
-                  </div>
-                </Form>
-              </Modal>
-            </Div>
-          </Container>
-          : null
-      }
-      <div style={{ display: "flex", gap: "15px", position: "relative", bottom: "-15px" }}>
+                  <button className="buttonCreateContente">
+                    Salvar alterações
+                  </button>
+                </div>
+              </Form>
+            </Modal>
+          </Div>
+        </Container>
+      ) : null}
+      <div
+        style={{
+          display: "flex",
+          gap: "15px",
+          position: "relative",
+          bottom: "-15px",
+        }}
+      >
         <Button
-          sx={
-            {
-              maxWidth: 80,
-              height: 38,
-              borderRadius: 1,
-              color: "#212529",
-              border: "2px solid #212529",
-            }
-          }
+          sx={{
+            maxWidth: 80,
+            height: 38,
+            borderRadius: 1,
+            color: "#212529",
+            border: "2px solid #212529",
+          }}
           onClick={() => setOpen(true)}
         >
           Editar
         </Button>
         <Button
-          sx={
-            {
-              maxWidth: 105,
-              height: 38,
-              fontFamily: "Inter",
-              fontStyle: "Semi Bold",
-              fontSize: 14,
-              borderRadius: 1,
-              color: "#212529",
-              border: "2px solid #212529",
-            }
-          }
+          sx={{
+            maxWidth: 105,
+            height: 38,
+            fontFamily: "Inter",
+            fontStyle: "Semi Bold",
+            fontSize: 14,
+            borderRadius: 1,
+            color: "#212529",
+            border: "2px solid #212529",
+          }}
           onClick={() => navigate(`/announcement/${annoucementId}`)}
         >
           Ver como
